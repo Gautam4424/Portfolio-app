6fa10df7bac36f2c71458f4e7063a63b9d780bb8
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-11-25 03:50:58 +0530

    Potfolio website

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+var typedData = new Typed(".role", {
+    strings: [
+        "Devops Engineer",
+        "Full Stack Developer",
+        "Web Developer",
+        "Coder",
+    ],
+    loop: true  ,
+    typeSpeed: 100,  
+    backSpeed: 80,  
+    backDelay: 1000,
+});
